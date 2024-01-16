d156cd66ca6a96226d023df9f81403f9fb5c41c9
Brian Di Palma <brian@env.brian-pop-os-pc-specialist>
2024-01-16 21:55:36 +0000

    feat(config): Add lazygit config

--- config/setup.sh
+++ config/setup.sh
@@ -3,4 +3,5 @@
 ln -sf ~/dev/iac/config/bat/ ~/.config/
 ln -sf ~/dev/iac/config/bottom/ ~/.config/
 ln -sf ~/dev/iac/config/fish/ ~/.config/
+ln -sf ~/dev/iac/config/lazygit/ ~/.config/
 ln -sf ~/dev/iac/config/lsd/ ~/.config/
