92719dfcdded155ff39a30bf63e575f3acbfd541
Brian Di Palma <brian@env.brian-pop-os-pc-specialist>
2024-01-16 22:10:50 +0000

    feat(config): Add kitty config

--- config/setup.sh
+++ config/setup.sh
@@ -3,6 +3,7 @@
 ln -sf ~/dev/iac/config/bat/ ~/.config/
 ln -sf ~/dev/iac/config/bottom/ ~/.config/
 ln -sf ~/dev/iac/config/fish/ ~/.config/
+ln -sf ~/dev/iac/config/kitty/ ~/.config/
 ln -sf ~/dev/iac/config/lazygit/ ~/.config/
 ln -sf ~/dev/iac/config/lsd/ ~/.config/
 ln -sf ~/dev/iac/config/nvim/ ~/.config/
