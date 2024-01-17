002488e3c55818a781a7747c992f92fa02f2c6d0
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2024-01-17 18:05:32 +0000

    feat(config-kitty): Link to workstation host config

--- config/setup.sh
+++ config/setup.sh
@@ -5,6 +5,7 @@ ln -sf ~/dev/iac/config/bottom/ ~/.config/
 ln -sf ~/dev/iac/config/fish/ ~/.config/
 ln -sf ~/dev/iac/config/git/ ~/.config/
 ln -sf ~/dev/iac/config/kitty/ ~/.config/
+ln -sf ~/dev/iac/config/kitty/brian-pop-os-pc-specialist.conf ~/.config/kitty/host-config.conf
 ln -sf ~/dev/iac/config/lazygit/ ~/.config/
 ln -sf ~/dev/iac/config/lsd/ ~/.config/
 ln -sf ~/dev/iac/config/nvim/ ~/.config/
