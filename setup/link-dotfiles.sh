bfe8ba83ccd5a0e861c2b4fdaf3eaa51beeea818
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2024-01-18 22:40:33 +0000

    refactor(dotfiles): Move to dotfiles directory

---
+++ setup/link-dotfiles.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+ln -sf ~/dev/iac/dotfiles/bat/ ~/.config/
+ln -sf ~/dev/iac/dotfiles/bottom/ ~/.config/
+ln -sf ~/dev/iac/dotfiles/fish/ ~/.config/
+ln -sf ~/dev/iac/dotfiles/git/ ~/.config/
+ln -sf ~/dev/iac/dotfiles/kitty/ ~/.config/
+ln -sf ~/dev/iac/dotfiles/kitty/brian-pop-os-pc-specialist.conf ~/.config/kitty/host-config.conf
+ln -sf ~/dev/iac/dotfiles/lazygit/ ~/.config/
+ln -sf ~/dev/iac/dotfiles/lsd/ ~/.config/
+ln -sf ~/dev/iac/dotfiles/nvim/ ~/.config/
+ln -sf ~/dev/iac/dotfiles/starship.toml ~/.config/
