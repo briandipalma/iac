d5f4b228c63ca6eab56500f4dc652c5abf6c7aa0
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2024-01-25 10:38:04 +0000

    feat(setup): Link kitty host config file

--- setup/link-dotfiles.sh
+++ setup/link-dotfiles.sh
@@ -1,12 +1,23 @@
 #! /bin/bash
 
+# Dotfiles
+
 ln -sf ~/dev/iac/dotfiles/bat/ ~/.config/
 ln -sf ~/dev/iac/dotfiles/bottom/ ~/.config/
 ln -sf ~/dev/iac/dotfiles/fish/ ~/.config/
 ln -sf ~/dev/iac/dotfiles/git/ ~/.config/
 ln -sf ~/dev/iac/dotfiles/kitty/ ~/.config/
-ln -sf ~/dev/iac/dotfiles/kitty/brian-pop-os-pc-specialist.conf ~/.config/kitty/host-config.conf
 ln -sf ~/dev/iac/dotfiles/lazygit/ ~/.config/
 ln -sf ~/dev/iac/dotfiles/lsd/ ~/.config/
 ln -sf ~/dev/iac/dotfiles/nvim/ ~/.config/
 ln -sf ~/dev/iac/dotfiles/starship.toml ~/.config/
+
+HOSTNAME=$(hostname)
+
+if [ -f ~/dev/iac/dotfiles/kitty/${HOSTNAME}.conf ]; then
+	echo "Linking kitty host config file for $HOSTNAME."
+	ln -sf ~/dev/iac/dotfiles/kitty/${HOSTNAME}.conf ~/.config/kitty/host-config.conf
+else
+	echo "Kitty host config file for $HOSTNAME does *not* exist, linking personal config."
+	ln -sf ~/dev/iac/dotfiles/kitty/personal.startup.conf ~/.config/kitty/host-config.conf
+fi
