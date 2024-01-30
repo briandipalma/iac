8839f88995a9caeb1525214ee02528d32c9d0d48
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2024-01-30 22:03:18 +0000

    feat(nvim): Disable auto-pairs

--- dotfiles/nvim/lua/plugins/disabled.lua
+++ dotfiles/nvim/lua/plugins/disabled.lua
@@ -1,6 +1,7 @@
 return {
-	{ "folke/tokyonight.nvim", enabled = false },
 	{ "catppuccin", enabled = false },
+	{ "echasnovski/mini.pairs", enabled = false },
+	{ "folke/tokyonight.nvim", enabled = false },
 	{ "lukas-reineke/indent-blankline.nvim", enabled = false },
 	{ "nvim-neo-tree/neo-tree.nvim", enabled = false },
 }
