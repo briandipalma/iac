b5a1864fbc97d1d2e597167d1ef63fd80b02119d
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2024-01-31 17:58:59 +0000

    feat(images): Move to bash script for setup
    It allows setting the container hostname to the local host one

---
+++ setup/images.sh
@@ -0,0 +1,9 @@
+distrobox rm env --force;
+
+distrobox create --image docker.io/briandipalma/env:latest \
+	--name env \
+	--pull \
+	--additional-flags "--hostname ${HOSTNAME}" \
+	--pre-init-hooks "SHELL=/usr/bin/fish";
+
+distrobox enter env;
