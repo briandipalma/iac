83a1c2a0e4094e92fe1316273f56721fea860178
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2024-02-06 16:33:21 +0000

    fix(fish): Add function header to mocks functions

--- dotfiles/fish/functions/start_mocks_core.fish
+++ dotfiles/fish/functions/start_mocks_core.fish
@@ -1,3 +1,5 @@
-podman login gitlab.caplin.com:5050 -u briand -p $(cat ~/dev/my-data/gitlab_secret)
-podman pull gitlab.caplin.com:5050/integration/fx-integration-api/mocks
-podman run --rm -p 18080:18080 -p 15001:15001 -p 15002:15002 gitlab.caplin.com:5050/integration/fx-integration-api/mocks start-core.sh
+function start_mocks_core
+    podman login gitlab.caplin.com:5050 -u briand -p $(cat ~/dev/my-data/gitlab_secret)
+    podman pull gitlab.caplin.com:5050/integration/fx-integration-api/mocks
+    podman run --rm -p 18080:18080 -p 15001:15001 -p 15002:15002 gitlab.caplin.com:5050/integration/fx-integration-api/mocks start-core.sh
+end
