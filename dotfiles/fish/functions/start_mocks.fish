cade170acbebdd9f54d7bf5a17315de0d45e88e7
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2024-02-06 15:10:03 +0000

    feat(fish): Add start mocks functions

---
+++ dotfiles/fish/functions/start_mocks.fish
@@ -0,0 +1,3 @@
+podman login gitlab.caplin.com:5050 -u briand -p $(cat ~/dev/my-data/gitlab_secret)
+podman pull gitlab.caplin.com:5050/integration/fx-integration-api/mocks
+podman run --rm -p 18080:18080 gitlab.caplin.com:5050/integration/fx-integration-api/mocks
