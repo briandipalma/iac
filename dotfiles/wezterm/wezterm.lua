1c211ca026ff6c7c46675360290bc3f8f62c6fc2
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2024-02-12 17:10:52 +0000

    feat(wezterm): Add launch programs

--- dotfiles/wezterm/wezterm.lua
+++ dotfiles/wezterm/wezterm.lua
@@ -3,6 +3,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.color_scheme = "rose-pine-dawn"
+config.font_size = 11.5
 config.hide_tab_bar_if_only_one_tab = true
 
 config.default_cwd = wezterm.home_dir .. "/dev/iac"
@@ -57,6 +58,33 @@ config.keys = {
 	{ key = "RightArrow", mods = "CTRL|SHIFT", action = wezterm.action.AdjustPaneSize({ "Right", 1 }) },
 	{ key = "UpArrow", mods = "CTRL|SHIFT", action = wezterm.action.AdjustPaneSize({ "Up", 1 }) },
 	{ key = "DownArrow", mods = "CTRL|SHIFT", action = wezterm.action.AdjustPaneSize({ "Down", 1 }) },
+	-- Launch programs
+	{
+		key = "F3",
+		action = wezterm.action.SpawnCommandInNewWindow({
+			args = { "distrobox", "enter", "env", "--", "nvim index.md && git pull" },
+			cwd = wezterm.home_dir .. "/dev/my-data/wiki/",
+		}),
+	},
+	{
+		key = "F5",
+		action = wezterm.action.SpawnCommandInNewWindow({
+			args = { "distrobox", "enter", "env", "--", "nvim && git pull" },
+			cwd = wezterm.home_dir .. "/dev/iac/",
+		}),
+	},
+	{
+		key = "F6",
+		action = wezterm.action.SpawnCommandInNewWindow({
+			args = { "distrobox", "enter", "env", "--", "btm" },
+		}),
+	},
+	{
+		key = "F7",
+		action = wezterm.action.SpawnCommandInNewWindow({
+			args = { "distrobox", "enter", "env", "--", "lazygit" },
+		}),
+	},
 }
 
 return config
