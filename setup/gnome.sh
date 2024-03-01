7a80f4af1d88b2fcaa8f1ffe5b6ceb54fc57a69f
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2024-03-01 22:34:18 +0000

    feat(gnome): Add tiling and dock settings

--- setup/gnome.sh
+++ setup/gnome.sh
@@ -13,6 +13,24 @@ dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-6 "['<Super>6'
 dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-7 "['<Super>7']"
 dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-8 "['<Super>8']"
 
+# Tiling
+
+dconf write /org/gnome/mutter/edge-tiling false
+dconf write /org/gnome/shell/extensions/pop-shell/tile-by-default true
+dconf write /org/gnome/shell/extensions/pop-shell/gap-inner "uint32 0"
+dconf write /org/gnome/shell/extensions/pop-shell/gap-outer "uint32 0"
+
+# Dock
+
+dconf write /org/gnome/shell/extensions/dash-to-dock/dock-fixed false
+dconf write /org/gnome/shell/extensions/dash-to-dock/intellihide true
+dconf write /org/gnome/shell/extensions/dash-to-dock/extend-height false
+
+# General
+
+dconf write /org/gnome/desktop/interface/show-battery-percentage true
+dconf write /org/gnome/nautilus/preferences/default-folder-viewer "'list-view'"
+
 # Custom keybindings
 
 dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/binding "'<Super>u'"
