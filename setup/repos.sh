e477d5185e3a4f5c033cc040f6cfa2fa5583fff6
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2024-03-05 11:11:52 +0000

    feat(git): Use credentials provider for 2FA repos

--- setup/repos.sh
+++ setup/repos.sh
@@ -8,13 +8,11 @@ if [ ! -d $DEV_DIR/my-data ]; then
 	git clone git@github.com:briandipalma/my-data.git $DEV_DIR/my-data
 fi
 
-token=$(cat $DEV_DIR/my-data/gitlab_secret)
-
 if [[ ! -d $DEV_DIR/m/mono && ${workWorkstations[@]} =~ $HOSTNAME ]]; then
-	git clone https://briand:${token}@gitlab.caplin.com/front-end/mono.git $DEV_DIR/m/mono
+	git clone https://gitlab.caplin.com/front-end/mono.git $DEV_DIR/m/mono
 	cp ./files/post-checkout $DEV_DIR/m/mono/.git/hooks/
 fi
 
 if [[ ! -d $DEV_DIR/fx-integration-api && ${workWorkstations[@]} =~ $HOSTNAME ]]; then
-	git clone https://briand:${token}@gitlab.caplin.com/integration/fx-integration-api.git $DEV_DIR/fx-integration-api
+	git clone https://gitlab.caplin.com/integration/fx-integration-api.git $DEV_DIR/fx-integration-api
 fi
