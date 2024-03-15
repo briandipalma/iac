af64dbcc7ad520b5482ee564efca5b390b1ef7cc
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2024-03-15 17:14:16 +0000

    fix(nvim): Update to close_fold_kinds_for_ft for nvim-ufo

--- dotfiles/nvim/lua/plugins/nvim-ufo.lua
+++ dotfiles/nvim/lua/plugins/nvim-ufo.lua
@@ -2,6 +2,9 @@ return {
 	"kevinhwang91/nvim-ufo",
 	dependencies = "kevinhwang91/promise-async",
 	opts = {
-		close_fold_kinds = { "imports", "comment" },
+		close_fold_kinds_for_ft = {
+			typescript = { "imports", "comment" },
+			typescriptreact = { "imports", "comment" },
+		},
 	},
 }
