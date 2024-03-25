06115cb1090f301d6fde5288ef1ba570a0fea3b2
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2024-03-25 17:40:45 +0000

    feat(fish): Updated gwb to use new branch name if source_branch is missing

--- dotfiles/fish/functions/gwb.fish
+++ dotfiles/fish/functions/gwb.fish
@@ -1,7 +1,14 @@
-# Branch a worktree from a non main/master branch
+# Create a worktree based on a specified branch
 # $argv[1] is the new branch
 # $argv[2] is the source branch
 function gwb
-    git worktree add -b $argv[1] ../$argv[1] origin/$argv[2]
-    cd ../$argv[1]
+    set new_branch $argv[1]
+    set source_branch $argv[2]
+
+    if not set -q source_branch
+        set source_branch $new_branch
+    end
+
+    git worktree add -b $new_branch ../$new_branch origin/$source_branch
+    cd ../$new_branch
 end
