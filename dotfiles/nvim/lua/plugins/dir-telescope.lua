1d215913b87016c3ca5eacbb38ae099c9c673f28
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2024-03-27 16:49:04 +0000

    feat(nvim): Remove dir-telescope
    fzf-native with refine fuzzy search provides equivalent functionality

--- dotfiles/nvim/lua/plugins/dir-telescope.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"princejoogie/dir-telescope.nvim",
-	config = true,
-	dependencies = "nvim-telescope/telescope.nvim",
-	keys = {
-		{ "<leader>fD", "<cmd>FileInDirectory<cr>", desc = "Find Files (user selected dir)" },
-		{ "<leader>sf", "<cmd>GrepInDirectory<cr>", desc = "Grep (user selected folder)" },
-	},
-}
