e709c187fc0a13a923df86494e821937f8270baa
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2024-04-02 09:20:27 +0100

    feat(kanata): Add esc <-> caps switch config

--- setup/links.sh
+++ setup/links.sh
@@ -9,6 +9,7 @@ ln -sf ~/dev/iac/dotfiles/bottom ~/.config/
 ln -sf ~/dev/iac/dotfiles/devilspie2 ~/.config/
 ln -sf ~/dev/iac/dotfiles/fish ~/.config/
 ln -sf ~/dev/iac/dotfiles/git ~/.config/
+ln -sf ~/dev/iac/dotfiles/kanata ~/.config/
 ln -sf ~/dev/iac/dotfiles/lazygit ~/.config/
 ln -sf ~/dev/iac/dotfiles/lsd ~/.config/
 ln -sf ~/dev/iac/dotfiles/nvim ~/.config/
