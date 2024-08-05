39a99268c8080219096de0c13b868891c9dfb0be
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2024-08-05 17:57:01 +0100

    feat(nvim): Add typescript compiler support to `:make`

---
+++ dotfiles/nvim/lua/config/autocmds.lua
@@ -0,0 +1,12 @@
+local augroup = vim.api.nvim_create_augroup("tsc-make", { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "typescript,typescriptreact",
+	group = augroup,
+	callback = function(args)
+		local project_root = string.gsub(args.file, "/src/.*", "")
+
+		vim.cmd.compiler("tsc")
+		vim.bo.makeprg = "npx tsc --project " .. project_root
+	end,
+})
