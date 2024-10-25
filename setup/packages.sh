fc9cad899972ac5ba8aa1ec3910685c7c87f07d2
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2024-10-25 15:27:37 +0100

    feat: Replace Kooha with OBS for screen recording

--- setup/packages.sh
+++ setup/packages.sh
@@ -5,7 +5,7 @@ source inventory.sh
 flatpak install -y flathub com.github.tchx84.Flatseal
 flatpak install -y flathub com.google.Chrome
 flatpak install -y flathub info.smplayer.SMPlayer
-flatpak install -y flathub io.github.seadve.Kooha
+flatpak install -y flathub com.obsproject.Studio
 flatpak install -y flathub org.remmina.Remmina
 flatpak install -y flathub org.virt_manager.virt-viewer
 flatpak install -y flathub org.wezfurlong.wezterm
@@ -14,46 +14,47 @@ sudo apt -y install yakuake
 
 # File type handling
 xdg-mime default info.smplayer.SMPlayer.desktop video/mp4
+xdg-mime default info.smplayer.SMPlayer.desktop video/x-matroska
 xdg-mime default com.github.taiko2k.tauonmb.desktop audio/flac
 
 if [[ ${workWorkstations[@]} =~ $HOSTNAME ]]; then
-	flatpak install -y flathub com.slack.Slack
+  flatpak install -y flathub com.slack.Slack
 fi
 
 if [[ ${personalWorkstations[@]} =~ $HOSTNAME ]]; then
-	flatpak install -y flathub com.discordapp.Discord
-	flatpak install -y flathub com.dropbox.Client
-	flatpak install -y flathub com.github.taiko2k.tauonmb
-	flatpak install -y flathub com.github.zocker_160.SyncThingy
-	flatpak install -y flathub com.heroicgameslauncher.hgl
-	flatpak install -y flathub com.valvesoftware.Steam
-	flatpak install -y flathub org.darktable.Darktable
-	flatpak install -y flathub org.kde.digikam
-	flatpak install -y flathub org.mozilla.Thunderbird
-	sudo apt -y install wireguard
+  flatpak install -y flathub com.discordapp.Discord
+  flatpak install -y flathub com.dropbox.Client
+  flatpak install -y flathub com.github.taiko2k.tauonmb
+  flatpak install -y flathub com.github.zocker_160.SyncThingy
+  flatpak install -y flathub com.heroicgameslauncher.hgl
+  flatpak install -y flathub com.valvesoftware.Steam
+  flatpak install -y flathub org.darktable.Darktable
+  flatpak install -y flathub org.kde.digikam
+  flatpak install -y flathub org.mozilla.Thunderbird
+  sudo apt -y install wireguard
 
-	if [ -d ~/wireguard/ ]; then
-		echo "Moving WireGuard config, invoking sudo"
-		sudo mv ~/wireguard /etc/
-		sudo chown -R root /etc/wireguard
-		sudo chgrp -R root /etc/wireguard
-		# Remove any group or others permissions, only owner (root) is able to read files
-		sudo chmod go= /etc/wireguard/private.key
-		sudo chmod go= /etc/wireguard/wg0.conf
-	fi
+  if [ -d ~/wireguard/ ]; then
+    echo "Moving WireGuard config, invoking sudo"
+    sudo mv ~/wireguard /etc/
+    sudo chown -R root /etc/wireguard
+    sudo chgrp -R root /etc/wireguard
+    # Remove any group or others permissions, only owner (root) is able to read files
+    sudo chmod go= /etc/wireguard/private.key
+    sudo chmod go= /etc/wireguard/wg0.conf
+  fi
 fi
 
 if [ ! -e /usr/bin/kanata ]; then
-	sudo curl --location --output /usr/bin/kanata https://github.com/jtroo/kanata/releases/download/v1.6.1/kanata
-	sudo chmod +x /usr/bin/kanata
+  sudo curl --location --output /usr/bin/kanata https://github.com/jtroo/kanata/releases/download/v1.6.1/kanata
+  sudo chmod +x /usr/bin/kanata
 
-	sudo groupadd uinput
-	sudo useradd -g uinput -G input kanata
+  sudo groupadd uinput
+  sudo useradd -g uinput -G input kanata
 
-	sudo cp ~/dev/iac/dotfiles/udev/60-uinput.rules /lib/udev/rules.d/
-	sudo cp ~/dev/iac/dotfiles/kanata/kanata.kbd /etc/kanata.kbd
+  sudo cp ~/dev/iac/dotfiles/udev/60-uinput.rules /lib/udev/rules.d/
+  sudo cp ~/dev/iac/dotfiles/kanata/kanata.kbd /etc/kanata.kbd
 
-	sudo cp ~/dev/iac/dotfiles/systemd/kanata.service /etc/systemd/system
-	sudo systemctl start kanata
-	sudo systemctl enable kanata
+  sudo cp ~/dev/iac/dotfiles/systemd/kanata.service /etc/systemd/system
+  sudo systemctl start kanata
+  sudo systemctl enable kanata
 fi
