499a41ac49560ed85ee90619cc24ed855e1bfbd2
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2024-11-24 22:41:10 +0000

    feat(inventory): Add new laptop

--- setup/inventory.sh
+++ setup/inventory.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 HOSTNAME=$(hostname)
-personalWorkstations=("brians-xps13-popos")
+personalWorkstations=("brians-xps13-popos" "brians-infinitybookpro14-popos")
 workWorkstations=("brian-pop-os-pc-specialist")
