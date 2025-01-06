96ba8dd519fcaa4ce444764941d20bfbd9049a67
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2025-01-06 17:33:35 +0000

    refactor(nvim): Remove indent-blankline plugin, replaced by snacks

--- dotfiles/nvim/lua/plugins/disabled.lua
+++ dotfiles/nvim/lua/plugins/disabled.lua
@@ -1,6 +1,5 @@
 return {
 	{ "catppuccin", enabled = false },
 	{ "folke/tokyonight.nvim", enabled = false },
-	{ "lukas-reineke/indent-blankline.nvim", enabled = false },
 	{ "nvim-neo-tree/neo-tree.nvim", enabled = false },
 }
