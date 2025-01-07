d4d59493969666d9bddb3ed0a6e699fa6988d2fb
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2025-01-07 17:38:47 +0000

    refactor(nvim): Remove telescope plugin config

--- dotfiles/nvim/lua/plugins/rose-pine.lua
+++ dotfiles/nvim/lua/plugins/rose-pine.lua
@@ -8,16 +8,6 @@ return {
 			styles = {
 				transparency = true,
 			},
-			-- Make telescope.nvim transparent
-			highlight_groups = {
-				NotifyBackground = { bg = "#000000" },
-				TelescopeBorder = { fg = "highlight_high", bg = "none" },
-				TelescopeNormal = { bg = "none" },
-				TelescopePromptNormal = { bg = "base" },
-				TelescopeResultsNormal = { fg = "subtle", bg = "none" },
-				TelescopeSelection = { fg = "text", bg = "base" },
-				TelescopeSelectionCaret = { fg = "rose", bg = "rose" },
-			},
 		},
 	},
 	{ "LazyVim/LazyVim", opts = { colorscheme = "rose-pine" } },
