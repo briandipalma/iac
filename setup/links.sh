440ea65f29637da3178370bb9cfc6c0fea9164f8
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2025-02-27 23:11:57 +0000

    feat(dotfiles): Add cosmic DE configuration

--- setup/links.sh
+++ setup/links.sh
@@ -4,6 +4,8 @@
 
 ln -sf ~/dev/iac/dotfiles/bat ~/.config/
 ln -sf ~/dev/iac/dotfiles/bottom ~/.config/
+rm -rf ~/.config/cosmic
+ln -sf ~/dev/iac/dotfiles/cosmic ~/.config/
 ln -sf ~/dev/iac/dotfiles/devilspie2 ~/.config/
 ln -sf ~/dev/iac/dotfiles/fish ~/.config/
 ln -sf ~/dev/iac/dotfiles/git ~/.config/
@@ -18,15 +20,15 @@ ln -sf ~/dev/iac/dotfiles/yakuakerc ~/.config/
 # Host specific configuration
 
 if [ -d ~/dev/iac/dotfiles/$HOSTNAME/autostart/ ]; then
-	ln -sf ~/dev/iac/dotfiles/$HOSTNAME/autostart ~/.config/
+  ln -sf ~/dev/iac/dotfiles/$HOSTNAME/autostart ~/.config/
 fi
 
 if [ -f ~/dev/my-data/$HOSTNAME/fish_history ]; then
-	mkdir -p ~/.local/share/fish
-	ln -sf ~/dev/my-data/$HOSTNAME/fish_history ~/.local/share/fish/fish_history
+  mkdir -p ~/.local/share/fish
+  ln -sf ~/dev/my-data/$HOSTNAME/fish_history ~/.local/share/fish/fish_history
 fi
 
 if [ -d ~/dev/my-data/$HOSTNAME/syncthing/ ]; then
-	mkdir -p ~/.var/app/com.github.zocker_160.SyncThingy/config
-	ln -sf ~/dev/my-data/$HOSTNAME/syncthing ~/.var/app/com.github.zocker_160.SyncThingy/config/
+  mkdir -p ~/.var/app/com.github.zocker_160.SyncThingy/config
+  ln -sf ~/dev/my-data/$HOSTNAME/syncthing ~/.var/app/com.github.zocker_160.SyncThingy/config/
 fi
