295634fb3c7b78f749721dd1bdff70f3dcf89a19
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2025-05-12 17:38:03 +0100

    chore(nvim): lazy-lock

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -3,3 +3,10 @@ if status is-interactive
 end
 
 starship init fish | source
+
+# pnpm
+set -gx PNPM_HOME "/home/brian/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
