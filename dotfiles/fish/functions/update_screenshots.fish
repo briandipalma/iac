e89d5e5d4edfea6333bcb4bfbbc99fdcf8cc5b47
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2025-05-23 14:42:53 +0100

    refactor(fish): Improve name of script variable

--- dotfiles/fish/functions/update_screenshots.fish
+++ dotfiles/fish/functions/update_screenshots.fish
@@ -1,6 +1,6 @@
 function update_screenshots
-    for actual_screenshots in (fd -E '*retry*' actual.png ~/Downloads/artifacts/)
-        set file_parts (string split \/ $actual_screenshots)
+    for actual_screenshot in (fd -E '*retry*' actual.png ~/Downloads/artifacts/)
+        set file_parts (string split \/ $actual_screenshot)
         set application $file_parts[7]
         set variant_browser (string split - $file_parts[10])
         set variant $variant_browser[1]
@@ -8,6 +8,6 @@ function update_screenshots
         set current_screenshot (string replace -r "\-actual" _$browser-ci $file_parts[11])
         set current_filepath (string join / apps $application pw-tests/tests $variant screenshots $current_screenshot)
 
-        cp $actual_screenshots $current_filepath
+        cp $actual_screenshot $current_filepath
     end
 end
