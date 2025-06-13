b6a0527d2be72ef559edaea9c229f9696966699a
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2025-06-13 17:46:29 +0100

    feat(nvim): Add keymap to delete scratch buffers

--- dotfiles/nvim/lua/plugins/snacks.lua
+++ dotfiles/nvim/lua/plugins/snacks.lua
@@ -4,5 +4,22 @@ return {
 		indent = {
 			enabled = false,
 		},
+		scratch = {
+			win = {
+				keys = {
+					["delete"] = {
+						"<a-x>",
+						function(self)
+							vim.api.nvim_win_call(self.win, function()
+								vim.cmd([[close]])
+								os.remove(vim.api.nvim_buf_get_name(self.buf))
+							end)
+						end,
+						desc = "Delete buffer",
+						mode = { "n", "x" },
+					},
+				},
+			},
+		},
 	},
 }
