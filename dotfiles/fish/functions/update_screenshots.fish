6effc8c97bcb94bc2d65b2bef5641f6d753a6ffa
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2025-10-23 22:48:32 +0100

    feat(update_screenshots): Unzip downloaded artifacts and delete them when done

--- dotfiles/fish/functions/update_screenshots.fish
+++ dotfiles/fish/functions/update_screenshots.fish
@@ -1,13 +1,18 @@
 function update_screenshots
-    for actual_screenshot in (fd -E '*retry*' actual.png ~/Downloads/artifacts/)
+    unzip ~/Downloads/artifacts.zip -d ~/Downloads/
+
+    for actual_screenshot in (fd -E '*retry*' actual.png ~/Downloads/apps/)
         set file_parts (string split \/ $actual_screenshot)
-        set application $file_parts[7]
-        set variant_browser (string split - $file_parts[10])
+        set application $file_parts[6]
+        set variant_browser (string split - $file_parts[9])
         set variant $variant_browser[1]
         set browser $variant_browser[-1]
-        set current_screenshot (string replace -r "\-actual" _$browser-ci $file_parts[11])
+        set current_screenshot (string replace -r "\-actual" _$browser-ci $file_parts[10])
         set current_filepath (string join / apps $application pw-tests/tests $variant screenshots $current_screenshot)
 
         cp $actual_screenshot $current_filepath
     end
+
+    rm -rf ~/Downloads/apps/
+    rm ~/Downloads/artifacts.zip
 end
