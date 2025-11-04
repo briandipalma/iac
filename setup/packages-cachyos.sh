3abd04ce9ea3e65026f1732145e68a10ac97f4e8
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2025-11-04 22:47:06 +0000

    feat(packages): Add WireGuard to CachyOS packages

--- setup/packages-cachyos.sh
+++ setup/packages-cachyos.sh
@@ -4,3 +4,20 @@ source inventory.sh
 
 # Main terminal
 sudo pacman -Sy kitty
+
+##
+## Personal workstations
+##
+if [[ ${personalWorkstations[@]} =~ $HOSTNAME ]]; then
+  sudo pacman -Sy wireguard-tools
+
+  if [ -d ~/wireguard/ ]; then
+    echo "Moving WireGuard config, invoking sudo"
+    sudo mv ~/wireguard /etc/
+    sudo chown -R root /etc/wireguard
+    sudo chgrp -R root /etc/wireguard
+    # Remove any group or others permissions, only owner (root) is able to read files
+    sudo chmod go= /etc/wireguard/private.key
+    sudo chmod go= /etc/wireguard/wg0.conf
+  fi
+fi
