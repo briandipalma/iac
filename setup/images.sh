d9b7fae9c1f10f63f8fedda6b08e028d720a2980
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2025-11-09 11:56:54 +0000

    fix(images): Move to docker to pull images

--- setup/images.sh
+++ setup/images.sh
@@ -2,15 +2,15 @@
 
 source inventory.sh
 
-podman image pull docker.io/briandipalma/base:latest
+docker image pull docker.io/briandipalma/base:latest
 distrobox assemble create --file ./base.ini
 
 if [[ ${personalWorkstations[@]} =~ $HOSTNAME ]]; then
-  podman image pull docker.io/briandipalma/admin:latest
+  docker image pull docker.io/briandipalma/admin:latest
   distrobox assemble create --file ./admin.ini
 fi
 
 if [[ ${workWorkstations[@]} =~ $HOSTNAME ]]; then
-  podman image pull docker.io/briandipalma/work:latest
+  docker image pull docker.io/briandipalma/work:latest
   distrobox assemble create --file ./work.ini
 fi
