0f5a4a37caf821fa82ebe1bb15836ad23c904a12
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2025-11-13 22:02:23 +0000

    feat: Add cachyos pcspecialist

--- setup/inventory.sh
+++ setup/inventory.sh
@@ -2,4 +2,4 @@
 
 HOSTNAME=$(hostname)
 personalWorkstations=("brians-xps13-popos" "brians-infinitybookpro14-popos" "brians-infinitybookpro14-cachyos")
-workWorkstations=("brian-pop-os-pc-specialist")
+workWorkstations=("brian-pop-os-pc-specialist", "briands-pcspecialist-cachyos")
