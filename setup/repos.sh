eacdb5b927557e86294d599edb47176fa8883d24
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2025-11-13 21:53:03 +0000

    feat(repos): Only clone proxmox on personal workstations

--- setup/repos.sh
+++ setup/repos.sh
@@ -5,18 +5,18 @@ source inventory.sh
 DEV_DIR=$HOME/dev
 
 if [ ! -d $DEV_DIR/my-data ]; then
-	git clone git@github.com:briandipalma/my-data.git $DEV_DIR/my-data
+  git clone git@github.com:briandipalma/my-data.git $DEV_DIR/my-data
 fi
 
-if [ ! -d $DEV_DIR/proxmox-services ]; then
-	git clone git@github.com:briandipalma/proxmox-services.git $DEV_DIR/proxmox-services
+if [[ ! -d $DEV_DIR/proxmox-services && ${personalWorkstations[@]} =~ $HOSTNAME ]]; then
+  git clone git@github.com:briandipalma/proxmox-services.git $DEV_DIR/proxmox-services
 fi
 
 if [[ ! -d $DEV_DIR/m/mono && ${workWorkstations[@]} =~ $HOSTNAME ]]; then
-	git clone https://gitlab.caplin.com/front-end/mono.git $DEV_DIR/m/mono
-	cp ./files/post-checkout $DEV_DIR/m/mono/.git/hooks/
+  git clone https://gitlab.caplin.com/front-end/mono.git $DEV_DIR/m/mono
+  cp ./files/post-checkout $DEV_DIR/m/mono/.git/hooks/
 fi
 
 if [[ ! -d $DEV_DIR/fx-integration-api && ${workWorkstations[@]} =~ $HOSTNAME ]]; then
-	git clone https://gitlab.caplin.com/integration/fx-integration-api.git $DEV_DIR/fx-integration-api
+  git clone https://gitlab.caplin.com/integration/fx-integration-api.git $DEV_DIR/fx-integration-api
 fi
