8bda7aefa1ce6cde1857404ac1fa058723e41d1d
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2025-11-14 15:17:03 +0000

    fix(repos): Make post-checkout executable

--- setup/repos-work.sh
+++ setup/repos-work.sh
@@ -7,6 +7,7 @@ DEV_DIR=$HOME/dev
 if [[ ! -d $DEV_DIR/m/mono && ${workWorkstations[@]} =~ $HOSTNAME ]]; then
   git clone https://gitlab.caplin.com/front-end/mono.git $DEV_DIR/m/mono
   cp ./files/post-checkout $DEV_DIR/m/mono/.git/hooks/
+  chmod u+x $DEV_DIR/m/mono/.git/hooks/post-checkout
 fi
 
 if [[ ! -d $DEV_DIR/fx-integration-api && ${workWorkstations[@]} =~ $HOSTNAME ]]; then
