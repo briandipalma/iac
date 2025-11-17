face39bf2eda9aeba24fe5a5e557d50a416b2b32
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2025-11-17 09:20:02 +0000

    fix(fish): Use trash-put only if present

--- dotfiles/fish/conf.d/aliases.fish
+++ dotfiles/fish/conf.d/aliases.fish
@@ -3,7 +3,9 @@ alias d="delta"
 alias g=git
 alias icat="flatpak run org.wezfurlong.wezterm imgcat"
 alias lg=lazygit
-alias rm=trash-put
+if type -q trash-put
+    alias rm=trash-put
+end
 alias s="ssh"
 alias y=yazi
 alias vim=nvim
