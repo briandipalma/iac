c535d102a2935bd8f19733b8aaf39e5f6af9c9ea
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2026-01-08 20:26:11 +0000

    feat(cachyos): Use paru for aur as it's preinstalled

--- setup/packages-cachyos.sh
+++ setup/packages-cachyos.sh
@@ -4,15 +4,13 @@ source inventory.sh
 
 # Main terminal
 sudo pacman -S kitty
-# AUR helper
-sudo pacman -S yay
 
 ##
 ## Personal workstations
 ##
 if [[ ${personalWorkstations[@]} =~ $HOSTNAME ]]; then
-  yay -S winbox
-  yay -S kanata-bin
+  paru -S winbox
+  paru -S kanata-bin
   source kanata.sh
 fi
 
@@ -20,10 +18,10 @@ fi
 ## Work workstations
 ##
 if [[ ${workWorkstations[@]} =~ $HOSTNAME ]]; then
-  yay -S netbird
-  yay -S fleet-desktop
+  paru -S netbird
+  paru -S fleet-desktop
 fi
 
 if [[ $HOSTNAME =~ "infinitybookpro14" ]]; then
-  yay -S yt6801-dkms
+  paru -S yt6801-dkms
 fi
