52c1e22cf47423f078ea784aaec6c306708232e5
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2026-01-09 15:36:02 +0000

    feat(nvim): Add own config show keys

--- dotfiles/nvim-own/plugin/30_show_keys.lua
+++ dotfiles/nvim-own/plugin/30_show_keys.lua
@@ -1,3 +1,54 @@
 MiniDeps.add("nvim-mini/mini.clue")
 
-require("mini.clue").setup()
+local miniclue = require("mini.clue")
+
+miniclue.setup({
+	triggers = {
+		-- Leader triggers
+		{ mode = { "n", "x" }, keys = "<Leader>" },
+
+		-- `[` and `]` keys
+		{ mode = "n", keys = "[" },
+		{ mode = "n", keys = "]" },
+
+		-- Built-in completion
+		{ mode = "i", keys = "<C-x>" },
+
+		-- `g` key
+		{ mode = { "n", "x" }, keys = "g" },
+
+		-- Marks
+		{ mode = { "n", "x" }, keys = "'" },
+		{ mode = { "n", "x" }, keys = "`" },
+
+		-- Registers
+		{ mode = { "n", "x" }, keys = '"' },
+		{ mode = { "i", "c" }, keys = "<C-r>" },
+
+		-- Window commands
+		{ mode = "n", keys = "<C-w>" },
+
+		-- `z` key
+		{ mode = { "n", "x" }, keys = "z" },
+	},
+
+	clues = {
+		-- Enhance this by adding descriptions for <Leader> mapping groups
+		miniclue.gen_clues.square_brackets(),
+		miniclue.gen_clues.builtin_completion(),
+		miniclue.gen_clues.g(),
+		miniclue.gen_clues.marks(),
+		miniclue.gen_clues.registers(),
+		miniclue.gen_clues.windows(),
+		miniclue.gen_clues.z(),
+	},
+
+	-- Clue window settings
+	window = {
+		--Floating window config
+		config = { width = "auto" },
+
+		-- Delay before showing clue window
+		delay = 0,
+	},
+})
