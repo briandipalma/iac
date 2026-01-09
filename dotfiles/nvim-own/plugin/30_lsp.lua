e8e078fe06c933a65a20c5acea51a68a1527b667
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2026-01-09 18:00:15 +0000

    feat(nvim): Organize imports

--- dotfiles/nvim-own/plugin/30_lsp.lua
+++ dotfiles/nvim-own/plugin/30_lsp.lua
@@ -31,3 +31,14 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		end
 	end,
 })
+
+vim.lsp.config("vtsls", {
+	on_attach = function(_, bufnr)
+		vim.api.nvim_buf_create_user_command(bufnr, "OrganizeImports", function()
+			vim.lsp.buf.code_action({
+				context = { only = { "source.organizeImports" }, diagnostics = {} },
+				apply = true,
+			})
+		end, { desc = "Organize imports using vtsls" })
+	end,
+})
