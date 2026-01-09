5946a7c5e5b971c2fd72cfccd475875023154463
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2026-01-09 12:01:41 +0000

    chore(nvim): Move own config to `plugin` directory

--- dotfiles/nvim-own/init.lua
+++ dotfiles/nvim-own/init.lua
@@ -1,5 +1,3 @@
-require("my-config.options")
-
 -- Clone 'mini.deps' in a way that it gets managed by 'mini.deps'
 local path_package = vim.fn.stdpath("data") .. "/site/"
 local mini_path = path_package .. "pack/deps/start/mini.deps"
@@ -20,8 +18,4 @@ if not vim.loop.fs_stat(mini_path) then
 	vim.cmd('echo "Installed [`mini.deps`](../doc/mini.deps.qmd#mini.deps)" | redraw')
 end
 
--- Set up 'mini.deps'
-require("mini.deps").setup({ path = { package = path_package } })
-
--- Import my plugins
-require("my-config.plugins.editor")
+require("mini.deps").setup()
