ffd83a1bc616a5841aadaa60feec2322e1b9aefc
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2026-01-13 13:43:50 +0000

    feat(nvim): Add organize imports keymap

--- dotfiles/nvim-own/after/lsp/vtsls.lua
+++ dotfiles/nvim-own/after/lsp/vtsls.lua
@@ -1,10 +1,12 @@
+local nml = require("my-config/utils").nml
+
 return {
 	on_attach = function(_, bufnr)
-		vim.api.nvim_buf_create_user_command(bufnr, "OrganizeImports", function()
+		nml("co", function()
 			vim.lsp.buf.code_action({
 				context = { only = { "source.organizeImports" }, diagnostics = {} },
 				apply = true,
 			})
-		end, { desc = "Organize imports using vtsls" })
+		end, { buffer = bufnr, desc = "Organize imports using vtsls" })
 	end,
 }
