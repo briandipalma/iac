162983fbfd438e7ab55e31f91a319368e5ba10c1
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2026-01-13 20:43:09 +0000

    feat(nvim): Add numbers to diagnostics

--- dotfiles/nvim-own/plugin/diagnostics.lua
+++ dotfiles/nvim-own/plugin/diagnostics.lua
@@ -1,5 +1,9 @@
 require("tiny-inline-diagnostic").setup({
-	options = { multilines = { enabled = true }, show_source = { enabled = true } },
+	options = {
+		add_messages = { display_count = true },
+		multilines = { enabled = true },
+		show_source = { enabled = true },
+	},
 })
 -- Disable Neovim's default virtual text diagnostics so there aren't duplicate diagnostics
 vim.diagnostic.config({ virtual_text = false })
