0477345972c09c539c9457b6c1ae047d63bf9503
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2026-01-13 21:06:52 +0000

    feat(nvim): Add diagnostic icons to own config

--- dotfiles/nvim-own/plugin/diagnostics.lua
+++ dotfiles/nvim-own/plugin/diagnostics.lua
@@ -1,9 +1,17 @@
 require("tiny-inline-diagnostic").setup({
 	options = {
-		add_messages = { display_count = true },
 		multilines = { enabled = true },
 		show_source = { enabled = true },
 	},
 })
--- Disable Neovim's default virtual text diagnostics so there aren't duplicate diagnostics
-vim.diagnostic.config({ virtual_text = false })
+
+vim.diagnostic.config({
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = " ",
+			[vim.diagnostic.severity.WARN] = " ",
+			[vim.diagnostic.severity.HINT] = " ",
+			[vim.diagnostic.severity.INFO] = " ",
+		},
+	},
+})
