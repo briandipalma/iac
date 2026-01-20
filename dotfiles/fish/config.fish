fce3cc9ee1901cf9826751c92282153b0203e131
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2026-01-20 15:15:24 +0000

    feat(pnpm): Use new version of pnpm

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -1,10 +1,3 @@
 if type -q starship
     starship init fish | source
 end
-
-# pnpm
-set -gx PNPM_HOME "/home/brian/.local/share/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-    set -gx PATH "$PNPM_HOME" $PATH
-end
-# pnpm end
