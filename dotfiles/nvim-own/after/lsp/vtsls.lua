6ece34ae0da71da536d68cc55a3c8517ba4053eb
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2026-01-21 13:44:16 +0000

    feat(nvim): Add file move/rename support to vtsls

--- dotfiles/nvim-own/after/lsp/vtsls.lua
+++ dotfiles/nvim-own/after/lsp/vtsls.lua
@@ -1,7 +1,36 @@
 local nml = require("my-config/utils").nml
 
 return {
-	on_attach = function(_, bufnr)
+	-- https://github.com/yioneko/vtsls/blob/main/packages/service/configuration.schema.json
+	settings = {
+		javascript = {
+			updateImportsOnFileMove = { enabled = "always" },
+		},
+		typescript = {
+			updateImportsOnFileMove = { enabled = "always" },
+			-- tsserver = { log = "verbose" }, -- For debugging server
+		},
+		vtsls = { autoUseWorkspaceTsdk = true },
+	},
+	on_attach = function(client, bufnr)
+		vim.api.nvim_create_autocmd("User", {
+			pattern = { "MiniFilesActionMove", "MiniFilesActionRename" },
+			callback = function(event)
+				local oldUri = vim.uri_from_fname(event.data.from)
+				local newUri = vim.uri_from_fname(event.data.to)
+				local changes = { files = { { oldUri = oldUri, newUri = newUri } } }
+
+				client.notify("workspace/didRenameFiles", changes)
+			end,
+		})
+
+		-- Comment back in to debug server
+		-- nml("cl", function()
+		-- 	client:exec_cmd({
+		-- 		command = "typescript.openTsServerLog",
+		-- 	}, { bufnr = bufnr })
+		-- end, { buffer = bufnr, desc = "Organize imports using vtsls" })
+
 		nml("co", function()
 			vim.lsp.buf.code_action({
 				context = { only = { "source.organizeImports" }, diagnostics = {} },
