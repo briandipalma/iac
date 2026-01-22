71efc99f37d7a7c8538ba93508b755adcee022f6
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2026-01-22 10:45:11 +0000

    feat(nvim): Notify for file types that are missing treesitter support

--- dotfiles/nvim-own/plugin/treesitter.lua
+++ dotfiles/nvim-own/plugin/treesitter.lua
@@ -6,7 +6,15 @@ treesitter.install({ "comment" })
 nml("it", "<Cmd>InspectTree<CR>", { desc = "Treesitter tree" })
 nml("iT", "<Cmd>Inspect<CR>", { desc = "Treesitter node under cursor" })
 
-local ignored = { ["fzf"] = true, ["fidget"] = true }
+local ignored = {
+	["blink-cmp-menu"] = true,
+	["codediff-explorer"] = true,
+	["flash_prompt"] = true,
+	["fidget"] = true,
+	["fzf"] = true,
+	["minideps-confirm"] = true,
+	["minifiles"] = true,
+}
 
 vim.api.nvim_create_autocmd("FileType", {
 	callback = function(_)
@@ -22,6 +30,6 @@ vim.api.nvim_create_autocmd("FileType", {
 			return
 		end
 
-		vim.print(_)
+		vim.notify("No treesitter support for file type " .. _.match .. " in file " .. _.file, vim.log.levels.INFO)
 	end,
 })
