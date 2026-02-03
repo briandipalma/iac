39b01603bec252fba1c413b4319c5b76722e82f7
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2026-02-03 16:56:00 +0000

    feat(nvim): Add last few missing plugins from old config

--- dotfiles/nvim-old/ftdetect/ansible.lua
+++
@@ -1,8 +0,0 @@
-vim.api.nvim_create_autocmd({ "BufRead" }, {
-	pattern = { "*.yml", "*.yaml" },
-	callback = function()
-		if vim.fn.search("- name: ") > 0 then
-			vim.bo.filetype = "yaml.ansible"
-		end
-	end,
-})
