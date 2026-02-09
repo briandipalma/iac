c512fa59b31740b51aa8b01f6bfa0d7e263b3583
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2026-02-09 22:32:23 +0000

    feat(nvim): Increase formatting timeout for ktfmt

--- dotfiles/nvim/plugin/formatting.lua
+++ dotfiles/nvim/plugin/formatting.lua
@@ -1,3 +1,4 @@
 local conform = require("conform")
 
-conform.setup({ format_on_save = { lsp_format = "fallback", timeout_ms = 500 } })
+-- ktfmt needs 1 second timeout to format
+conform.setup({ format_on_save = { lsp_format = "fallback", timeout_ms = 1000 } })
