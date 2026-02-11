5437d10bba8c20ccb17844995236e612e46f4b35
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2026-02-11 12:52:34 +0000

    feat(nvim): Add formatting info keymap

--- dotfiles/nvim/plugin/formatting.lua
+++ dotfiles/nvim/plugin/formatting.lua
@@ -1,4 +1,7 @@
 local conform = require("conform")
+local nml = require("my-config/utils").nml
 
 -- ktfmt needs 1 second timeout to format
 conform.setup({ format_on_save = { lsp_format = "fallback", timeout_ms = 1000 } })
+
+nml("if", "<Cmd>ConformInfo<CR>", { desc = "Formatting logs" })
