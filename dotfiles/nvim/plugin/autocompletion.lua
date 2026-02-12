f25b034e43a93a971667ed14b9edc73ffbcb49e5
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2026-02-12 11:45:57 +0000

    feat(nvim): Add cmdline autocompletion support

--- dotfiles/nvim/plugin/autocompletion.lua
+++ dotfiles/nvim/plugin/autocompletion.lua
@@ -1,13 +1,22 @@
 require("blink.cmp").setup({
-	signature = { enabled = true },
+	cmdline = { completion = { menu = { auto_show = true } } },
 	completion = {
 		documentation = { auto_show = true, auto_show_delay_ms = 500 },
+		ghost_text = { enabled = true },
 		menu = {
 			auto_show = true,
+			cmdline_position = function()
+				if vim.g.ui_cmdline_pos ~= nil then
+					local pos = vim.g.ui_cmdline_pos
+					return { pos[1] - 1, pos[2] + 1 } -- Tweak position of autocomplete menu for noice.nvim cmdline
+				end
+				local height = (vim.o.cmdheight == 0) and 1 or vim.o.cmdheight
+				return { vim.o.lines - height, 0 }
+			end,
 			draw = {
 				treesitter = { "lsp" },
-				columns = { { "kind_icon", "label", "label_description", gap = 1 }, { "kind" } },
 			},
 		},
 	},
+	signature = { enabled = true },
 })
