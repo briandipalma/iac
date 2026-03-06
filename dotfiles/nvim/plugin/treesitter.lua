27ce53a464ce7a968993a851c546ad4bbd624d69
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2026-03-06 18:07:53 +0000

    feat(nvim): Only allow 3 lines of treesitter-context

--- dotfiles/nvim/plugin/treesitter.lua
+++ dotfiles/nvim/plugin/treesitter.lua
@@ -1,6 +1,8 @@
 local nml = require("my-config/utils").nml
 local treesitter = require("nvim-treesitter")
+local context = require("treesitter-context")
 
+context.setup({ max_lines = 3 })
 treesitter.install({ "comment" })
 
 nml("it", "<Cmd>InspectTree<CR>", { desc = "Treesitter tree" })
