c8aed86f226eeaadc073939cafaf07b0635db377
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2026-03-27 14:29:18 +0000

    feat(nvim): Add buffer formatting toggle keybind

--- dotfiles/nvim/plugin/formatting.lua
+++ dotfiles/nvim/plugin/formatting.lua
@@ -1,7 +1,18 @@
 local conform = require("conform")
 local nml = require("my-config/utils").nml
 
--- ktfmt needs 1 second timeout to format
-conform.setup({ format_on_save = { lsp_format = "fallback", timeout_ms = 1000 } })
+conform.setup({
+	format_on_save = function(bufnr)
+		if vim.b[bufnr].disable_autoformat then
+			return
+		end
+
+		-- ktfmt needs 1 second timeout to format
+		return { lsp_format = "fallback", timeout_ms = 1000 }
+	end,
+})
 
 nml("if", "<Cmd>ConformInfo<CR>", { desc = "Formatting logs" })
+nml("uf", function()
+	vim.b.disable_autoformat = not vim.b.disable_autoformat
+end, { desc = "Toggle buffer formatting" })
