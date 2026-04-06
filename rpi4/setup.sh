8ab272fdb84674dddba80b9b06b80ce1a857c233
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2026-04-06 17:44:28 +0100

    feat(rpi4): Add ssh public keys and storage setup

--- rpi4/setup.sh
+++ rpi4/setup.sh
@@ -1,7 +1,13 @@
 #! /bin/bash
 
+ln -sf ~/iac/rpi4/authorized_keys ~/.ssh/authorized_keys
+
 git config --local credential.helper "store --file .git-credentials"
 git config --global user.email "1597820+briandipalma@users.noreply.github.com"
 git config --global user.name "Brian Di Palma"
 
 hostnamectl hostname rpi4.test
+
+mkdir -p /mnt/storage
+sudo chown data:data /mnt/storage/
+mkdir -p /mnt/storage/appdata
