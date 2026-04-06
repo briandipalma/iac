2f3b56d00af3d7fa9986737efb600828d79395a9
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2026-04-06 16:43:09 +0100

    feat(rpi4): Add setup script

---
+++ rpi4/setup.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+git config --local credential.helper "store --file .git-credentials"
+git config --global user.email "1597820+briandipalma@users.noreply.github.com"
+git config --global user.name "Brian Di Palma"
