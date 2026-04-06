447584f29d0a43681c2f943ef5ccaec9d5b0990b
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2026-04-06 17:19:21 +0100

    feat(rpi4): Set hostname in setup script

--- rpi4/setup.sh
+++ rpi4/setup.sh
@@ -3,3 +3,5 @@
 git config --local credential.helper "store --file .git-credentials"
 git config --global user.email "1597820+briandipalma@users.noreply.github.com"
 git config --global user.name "Brian Di Palma"
+
+hostnamectl hostname rpi4.test
