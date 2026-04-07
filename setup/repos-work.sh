5fa9e42c8cd5f6ff29c7c8ff817b7b1cee7e0395
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2026-04-07 10:40:49 +0100

    feat(repos-work): Change all work repos to use worktrees

--- setup/repos-work.sh
+++ setup/repos-work.sh
@@ -4,12 +4,12 @@ source inventory.sh
 
 DEV_DIR=$HOME/dev
 
-if [[ ! -d $DEV_DIR/m/mono && ${workWorkstations[@]} =~ $HOSTNAME ]]; then
-  git clone https://gitlab.caplin.com/front-end/mono.git $DEV_DIR/m/mono
-  cp ./files/post-checkout $DEV_DIR/m/mono/.git/hooks/
-  chmod u+x $DEV_DIR/m/mono/.git/hooks/post-checkout
+if [[ ! -d $DEV_DIR/mono/master && ${workWorkstations[@]} =~ $HOSTNAME ]]; then
+  git clone https://gitlab.caplin.com/front-end/mono.git $DEV_DIR/mono/master
+  cp ./files/post-checkout $DEV_DIR/mono/master/.git/hooks/
+  chmod u+x $DEV_DIR/mono/master/.git/hooks/post-checkout
 fi
 
-if [[ ! -d $DEV_DIR/fx-integration-api && ${workWorkstations[@]} =~ $HOSTNAME ]]; then
-  git clone https://gitlab.caplin.com/integration/fx-integration-api.git $DEV_DIR/fx-integration-api
+if [[ ! -d $DEV_DIR/fx-integration-api/main && ${workWorkstations[@]} =~ $HOSTNAME ]]; then
+  git clone https://gitlab.caplin.com/integration/fx-integration-api.git $DEV_DIR/fx-integration-api/main
 fi
