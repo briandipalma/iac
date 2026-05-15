0a8dfd0b68090f4246881f205063a1da9fa7a89a
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2026-05-15 17:54:41 +0100

    feat(fish): Fix change_scale so it runs from niri keybind

--- dotfiles/fish/functions/change_scale.fish
+++ dotfiles/fish/functions/change_scale.fish
@@ -1,14 +1,13 @@
 #!/usr/bin/fish
 
-function change_scale
-    set focused_ouput $(niri msg focused-output | awk -F'[()]' 'NR==1 {print $2}')
-    set current_scale $(niri msg --json outputs | jq -r ".[\"$focused_ouput\"].logical.scale")
+set focused_output (niri msg --json focused-output)
+set output_name (echo $focused_output | jq -r '.name')
+set current_scale (echo $focused_output | jq -r '.logical.scale')
 
-    if test "$argv[1]" = down
-        set new_scale (math $current_scale - 0.1)
-    else
-        set new_scale (math $current_scale + 0.1)
-    end
-
-    niri msg output "$focused_ouput" scale "$new_scale"
+if test "$argv[1]" = down
+    set new_scale (math $current_scale - 0.1)
+else
+    set new_scale (math $current_scale + 0.1)
 end
+
+niri msg output "$output_name" scale "$new_scale"
