66156f6af50d6e90a7a9f0ca520a8182cae689bd
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2026-07-08 18:02:04 +0100

    fix(git): Move to `main` from `master`

--- setup/repos-work.sh
+++ setup/repos-work.sh
@@ -4,10 +4,10 @@ source inventory.sh
 
 DEV_DIR=$HOME/dev
 
-if [[ ! -d $DEV_DIR/mono/master && ${workWorkstations[@]} =~ $HOSTNAME ]]; then
-  git clone https://gitlab.caplin.com/front-end/mono.git $DEV_DIR/mono/master
-  cp ./files/post-checkout $DEV_DIR/mono/master/.git/hooks/
-  chmod u+x $DEV_DIR/mono/master/.git/hooks/post-checkout
+if [[ ! -d $DEV_DIR/mono/main && ${workWorkstations[@]} =~ $HOSTNAME ]]; then
+  git clone https://gitlab.caplin.com/front-end/mono.git $DEV_DIR/mono/main
+  cp ./files/post-checkout $DEV_DIR/mono/main/.git/hooks/
+  chmod u+x $DEV_DIR/mono/main/.git/hooks/post-checkout
 fi
 
 if [[ ! -d $DEV_DIR/fx-integration-api/main && ${workWorkstations[@]} =~ $HOSTNAME ]]; then
