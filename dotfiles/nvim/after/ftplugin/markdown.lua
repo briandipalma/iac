d7043cd5e79f912c86c6af529567030d228c0ff1
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2026-09-14 16:30:32 +0100

    Fix LSP hovers cutting off text

--- dotfiles/nvim/after/ftplugin/markdown.lua
+++ dotfiles/nvim/after/ftplugin/markdown.lua
@@ -3,9 +3,8 @@ local install_package = require("my-config/utils").install_package
 local lint = require("lint")
 local treesitter = require("nvim-treesitter")
 
-vim.wo.conceallevel = 2 -- Hide most symbols for cleaner looking documentation
-vim.wo.spell = true -- Enable spell checking
-vim.wo.wrap = false -- Wrapping can make tables etc hard to read, `markview.nvim` recommends it off
+-- Conceallevel is great for rendering clean docs in both files and hover windows
+vim.wo.conceallevel = 2
 
 treesitter.install({ "markdown" })
 vim.treesitter.start()
@@ -13,12 +12,21 @@ vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.wo[0][0].foldmethod = "expr"
 vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 
-install_package("marksman")
-install_package("ltex-ls-plus")
-vim.lsp.enable("marksman")
-vim.lsp.enable("ltex_plus")
+-- Only configure spellcheck, nowrap, LSP servers, and linters/formatters for real markdown files
+local is_real_file = vim.bo.buftype == ""
 
-install_package("prettier")
-conform.formatters_by_ft.markdown = { "prettier" }
+if is_real_file then
+	vim.wo.spell = true -- Enable spell checking
+	vim.wo.wrap = false -- Wrapping can make tables etc hard to read, `markview.nvim` recommends it off
 
-require("lint").try_lint("markdownlint-cli2")
+	install_package("marksman")
+	install_package("ltex-ls-plus")
+	vim.lsp.enable("marksman")
+	vim.lsp.enable("ltex_plus")
+
+	install_package("prettier")
+	conform.formatters_by_ft.markdown = { "prettier" }
+
+	install_package("markdownlint-cli2")
+	lint.try_lint("markdownlint-cli2")
+end
